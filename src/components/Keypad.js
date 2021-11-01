8f7b7cf0eeb98208d5d61eb965be54b639521079
yatsun <yatsunlam25@gmail.com>
2021-11-01 00:48:36 -0400

    commit tree 7c4b866f147522d4faedbfc5ef055ec1f56c0bd0

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
 // Code Keypad Component Here
+import React, { Component } from 'react';
+
+export default class KeyPad extends Component {
+
+    keyPressCallback = () => {
+        console.log('Entering password...')
+    }
+
+
+    render() {
+        return(
+            <input type="password" onKeyUp={this.keyPressCallback}></input> // only keyup works. keyPress and keyDown doesn't work
+        )
+    }
+}
